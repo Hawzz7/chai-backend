46a0d95a3b134cc37dc7a8cc66e90a4daf1e5cb1
Hawzz7 <vicky.007.cr@gmail.com>
2024-03-16 20:21:57 +0530

    add user and video model

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
+//origin: process.env.CORS_ORIGIN: This option specifies the allowed origins for cross-origin requests. process.env.CORS_ORIGIN likely retrieves the value from an environment variable. The CORS_ORIGIN environment variable should contain the allowed origin(s) for cross-origin requests. If the value of CORS_ORIGIN is '*', it allows requests from any origin. If it's a specific origin (e.g., 'http://example.com'), it allows requests only from that origin.
 
 app.use(express.json({limit: "16kb"}))//controls the data size or Controls the maximum request body size. If this is a number, then the value specifies the number of bytes; if it is a string, the value is passed to the bytes library for parsing. Defaults to '100kb'.
 
---
+++ src/models/user.model.js
@@ -0,0 +1,99 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary url will be stored
+            required: true,
+        },
+        coverImage: {
+            type: String
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+//see mongoosejs.com for more info on 'pre'
+//inside 'pre' don't use arrow functions as callback function use the normal function syntax as callback function  
+userSchema.pre("save", async function (next) {
+    //here it will check if the password is modified then only it will run the bcypt.hash line 60
+    if (!this.isModified("password")) return next()
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+
+//Just like middleware, Mongoose allows you to define custom methods on your models. These methods can perform specific tasks or operations related to your data model. Custom methods are useful for encapsulating logic that is specific to your application's requirements.
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}//this function will return true false after comparing passwords from db
+
+
+userSchema.methods.generateAccessToken = function () {//we can use async await in this middleware but generally it is not used as the function runs very quickly
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {//expiryToken always inside an object
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {//expiryToken always inside an object
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
