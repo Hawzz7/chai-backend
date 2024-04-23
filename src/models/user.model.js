b6ccad9f104011a0b9fa1230698262a67c97e195
Hawzz7 <vicky.007.cr@gmail.com>
2024-04-23 10:44:41 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,7 +57,7 @@ userSchema.pre("save", async function (next) {
     //here it will check if the password is modified then only it will run the bcypt.hash line 60
     if (!this.isModified("password")) return next()
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
