78b13882db1c94b20a54d821c22c621f7bc5fe50
Hawzz7 <vicky.007.cr@gmail.com>
2024-05-21 22:20:26 +0530

    add user account details management endpoints

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,11 +16,12 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        //file has been uploaded successfully
+        //file has been uploaded successfully to cloudinary
         // console.log("file uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
+        fs.unlinkSync(localFilePath) //will remove the file from server(localstorage)
+        
         return response;
-    }//upto this line the localFilePath has been uploaded to the server(localstorage) but not uploaded to cloudinary so it create some malicious file which are harmful therefore it is necessary to remove the file from server
+    }//upto this line the localFilePath has been uploaded to the server(localstorage) and also to cloudinary. But it create some malicious file in the server(localstorage) which are harmful therefore it is necessary to remove the file from server
     catch (error) {
         fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operation got failed
         return null;
